6d6148f0e1b17b9705eb4c04798c569af17b094f
Gradaraks <134728756+Gradaraks@users.noreply.github.com>
2026-04-24 14:37:52 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,8 +1,14 @@
+// src/db.js
 const { Pool } = require('pg');
+
+if (!process.env.DATABASE_URL) {
+  console.error('❌ Ошибка: переменная окружения DATABASE_URL не установлена!');
+  process.exit(1);
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false }  // обязательно для Railway
 });
 
 module.exports = pool;
